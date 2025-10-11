b7134eff42b62dd504398f065082801f6d27b147
jogen12 <veliarkekovich@gmail.com>
2025-10-11 10:37:57 +0300

    feat: update scripts format

--- config/options.nix
+++ config/options.nix
@@ -119,5 +119,17 @@
         end
       '';
     }
+    {
+      event = ["BufRead" "BufNewFile"];
+      pattern = ["*.sh" "*.bash"];
+      callback.__raw = ''
+        function()
+          vim.opt_local.expandtab   = true
+          vim.opt_local.tabstop     = 4
+          vim.opt_local.shiftwidth  = 4
+          vim.opt_local.softtabstop = 4
+        end
+      '';
+    }
   ];
 }
